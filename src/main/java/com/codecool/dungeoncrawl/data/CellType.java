b35c3c2868a0902b61ac14357fdda5e8711a6f3b
tomatams <tomatams@gmail.com>
2023-04-03 13:01:24 +0200

    add health and key CellType to Celltypes

--- src/main/java/com/codecool/dungeoncrawl/data/CellType.java
+++ src/main/java/com/codecool/dungeoncrawl/data/CellType.java
@@ -3,7 +3,9 @@ package com.codecool.dungeoncrawl.data;
 public enum CellType {
     EMPTY("empty"),
     FLOOR("floor"),
-    WALL("wall");
+    WALL("wall"),
+    HEALTH("health"),
+    key("key");
 
     private final String tileName;
 
