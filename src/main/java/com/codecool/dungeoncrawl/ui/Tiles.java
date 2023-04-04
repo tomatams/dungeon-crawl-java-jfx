17c722ad5c76defb74c025f637539a0f42ba98e5
tomatams <tomatams@gmail.com>
2023-04-04 11:46:39 +0200

    display items

--- src/main/java/com/codecool/dungeoncrawl/ui/Tiles.java
+++ src/main/java/com/codecool/dungeoncrawl/ui/Tiles.java
@@ -28,7 +28,7 @@ public class Tiles {
         tileMap.put("floor", new Tile(2, 0));
         tileMap.put("player", new Tile(27, 0));
         tileMap.put("skeleton", new Tile(29, 6));
-        tileMap.put("health", new Tile(26, 23));
+        tileMap.put("potion", new Tile(26, 23));
         tileMap.put("key", new Tile(16, 23));
     }
 
