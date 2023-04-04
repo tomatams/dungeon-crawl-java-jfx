7331b22a60f3effcdc1e6e65842b4043f0381d5b
Le Marcell <73469368+felhos@users.noreply.github.com>
2023-04-04 13:17:11 +0200

    add new constructor to actors

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Actor.java
@@ -7,10 +7,18 @@ import com.codecool.dungeoncrawl.data.Drawable;
 public abstract class Actor implements Drawable {
     private Cell cell;
     private int health = 10;
+    private int damage = 2;
 
     public Actor(Cell cell) {
         this.cell = cell;
         this.cell.setActor(this);
+    }
+
+    public Actor(Cell cell, int health, int damage) {
+        this.cell = cell;
+        this.cell.setActor(this);
+        this.health = health;
+        this.damage = damage;
     }
 
     public void move(int dx, int dy) {
--- src/main/java/com/codecool/dungeoncrawl/data/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Skeleton.java
@@ -3,8 +3,10 @@ package com.codecool.dungeoncrawl.data.actors;
 import com.codecool.dungeoncrawl.data.Cell;
 
 public class Skeleton extends Actor {
+    private final static int baseHealth = 10;
+    private final static int baseDamage = 2;
     public Skeleton(Cell cell) {
-        super(cell);
+        super(cell, baseHealth, baseDamage);
     }
 
     @Override
