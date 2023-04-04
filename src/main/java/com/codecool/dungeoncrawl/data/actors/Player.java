6fc3d257a2e11f4e5649bd493d76d4906ed9f861
tomatams <tomatams@gmail.com>
2023-04-04 12:04:51 +0200

    add itemList to player

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
@@ -1,13 +1,26 @@
 package com.codecool.dungeoncrawl.data.actors;
 
 import com.codecool.dungeoncrawl.data.Cell;
+import com.codecool.dungeoncrawl.data.items.Item;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Player extends Actor {
+    List<Item> itemList;
     public Player(Cell cell) {
         super(cell);
+        itemList = new ArrayList<>();
     }
 
     public String getTileName() {
         return "player";
+    }
+
+    public void pickUpItem (Item item) {
+        itemList.add(item);
+    }
+    public List<Item> getItemList() {
+        return itemList;
     }
 }
