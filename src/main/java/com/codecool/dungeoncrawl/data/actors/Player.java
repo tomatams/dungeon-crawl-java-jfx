3450079a92b6d1110166562313d3dcf52e82a654
tomatams <tomatams@gmail.com>
2023-04-05 09:57:47 +0200

    add playerName field

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Player.java
@@ -8,6 +8,8 @@ import java.util.Map;
 
 public class Player extends Actor {
 
+    private String playerName;
+
     private Map<String, Integer> itemList;
     private final static int BASE_HEALTH = 15;
     private final static int BASE_DAMAGE = 5;
