425f40c6d3b504ad9a8be9e48b22081ce1bddb84
Le Marcell <73469368+felhos@users.noreply.github.com>
2023-04-05 10:50:29 +0200

    refactor skeleton class

--- src/main/java/com/codecool/dungeoncrawl/data/actors/Enemy.java
+++ src/main/java/com/codecool/dungeoncrawl/data/actors/Enemy.java
@@ -2,7 +2,17 @@ package com.codecool.dungeoncrawl.data.actors;
 
 import com.codecool.dungeoncrawl.data.Cell;
 
+import java.util.Random;
+
 public abstract class Enemy extends Actor{
+    int[][] directions;
+    Random random;
+
+    public void randomMove() {
+        int[][] directions = {{1,0},{0,1},{0,-1},{-1,0},{0,0}};
+        int[] randomDirection = directions[random.nextInt(directions.length)];
+        super.move(randomDirection[0],randomDirection[1]);
+    }
 
     public Enemy(Cell cell) {
         super(cell);
